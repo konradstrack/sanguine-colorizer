7b0d58ff840cd96b8c8552691dc20c6961b1622c
Konrad Strack <konrad@strack.pl>
2014-07-14 00:54:21 +0200

    a simple window, sinatra assetpack

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'rack'
 gem 'sinatra', '~> 1.4.5'
+gem 'sinatra-assetpack'
+gem 'sass'
--- themer.rb
+++ themer.rb
@@ -1,6 +1,21 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/assetpack'
 
 class SanguineThemer < Sinatra::Base
+	set :root, File.dirname(__FILE__)
+
+	register Sinatra::AssetPack
+	assets do
+		serve '/css', from: 'app/css'
+
+		css :application, '/css/application.css', [
+			'/css/layout.css',
+			'/css/sanguine.css'
+		]
+
+#		css_compression :sass
+	end
+
 	get '/sanguine' do
 		erb :sanguine
 	end
